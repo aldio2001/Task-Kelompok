13f588050b7ab4547ae1e63243eb95162f41ac66
Aldio Yohanes <aldioguire@gmail.com>
2024-06-15 23:25:30 +0700

    Lanjutkan disini alarm manager nya ester

--- app/src/main/java/com/eritlab/jexmon/presentation/screens/notification_screen/component/NotificationScreen.kt
+++ app/src/main/java/com/eritlab/jexmon/presentation/screens/notification_screen/component/NotificationScreen.kt
@@ -1,18 +1,30 @@
 package com.eritlab.jexmon.presentation.screens.notification_screen.component
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun NotificationScreen() {
+    val context = LocalContext.current
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text="Notification Screen")
+        Text(text = "Notification Screen")
+
+        Button(onClick = {
+            // Aksi yang dilakukan saat tombol diklik
+            Toast.makeText(context, "Button clicked", Toast.LENGTH_SHORT).show()
+        }) {
+            Text(text = "Alarm manager")
+        }
     }
 }
